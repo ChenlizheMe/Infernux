a56a6e5537ae32be1692437be19960455cf24733
LizheChen <574118090@qq.com>
2026-09-17 04:17:08 +0800

    test: accept absolute scenes in raycast benchmark

--- scripts/acceptance/raycast_benchmark.py
+++ scripts/acceptance/raycast_benchmark.py
@@ -64,8 +64,13 @@ def _parser() -> argparse.ArgumentParser:
 def main() -> int:
     args = _parser().parse_args()
     project = resolved_path(args.project)
-    scene_relative = str(args.scene).replace("\\", "/")
-    scene_path = resolved_path(os.path.join(project, *scene_relative.split("/")))
+    scene_argument = os.path.expandvars(os.path.expanduser(str(args.scene)))
+    if os.path.isabs(scene_argument):
+        scene_path = resolved_path(scene_argument)
+        scene_relative = os.path.relpath(scene_path, project).replace("\\", "/")
+    else:
+        scene_relative = scene_argument.replace("\\", "/")
+        scene_path = resolved_path(os.path.join(project, *scene_relative.split("/")))
     if not os.path.isfile(scene_path):
         raise FileNotFoundError(scene_path)
     counts = tuple(sorted({int(item) for item in str(args.counts).split(",") if item}))
